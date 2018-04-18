8564c225dc3cdd3b863c42b42429703c31827c46
hsi0 <37262554+hsi0@users.noreply.github.com>
2018-04-18 17:17:26 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace joclinii5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button1.Font = new System.Drawing.Font("Chiller", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(12, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button3.Font = new System.Drawing.Font("Chiller", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Transparent;
+            this.button3.Location = new System.Drawing.Point(12, 269);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(84, 34);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Iesire";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Chiller", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(165, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(490, 258);
+            this.label1.TabIndex = 6;
+            this.label1.Text = resources.GetString("label1.Text");
+            this.label1.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(114, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(934, 724);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button2.Font = new System.Drawing.Font("Chiller", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button2.Location = new System.Drawing.Point(12, 191);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 28);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Help";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(1028, 725);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace joclinii5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        const int nmax = 18;
+        int Jucator = 0;
+        int[,] tab = new int[nmax + 1, nmax + 1];
+        Graphics g;
+        [DllImport("kernel32.dll")]
+        public static extern bool Beep(int freq, int duration);
+
+        private System.Media.SoundPlayer mediaSoundPlayer = new System.Media.SoundPlayer();
+        public void PlaySound(string path)
+        {
+            
+
+           // aPath = Application.StartupPath + "\\wav\\" + wavName.Trim();
+           if (File.Exists(path))
+            {
+                mediaSoundPlayer.SoundLocation = path;
+                mediaSoundPlayer.Play();
+            }
+            else
+                throw new Exception("Invalid sound file!");
+        }
+
+     /*   public void StopSound()
+        {
+            try
+            {
+                mediaSoundPlayer.Stop();
+            }
+            catch { }
+
+        }*/
+        private void initab()
+        {
+            int i, j;
+            for (i = 1; i <= nmax; i++)
+                for (j = 1; j <= nmax; j++)
+                    tab[i, j] = 0;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Beep(100, 300))
+            {
+
+            }
+       
+        
+            initab();
+            Desenare();
+        }
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (Beep(100, 300))
+            {
+
+            }
+            g.Clear(Color.Black);
+            label1.Visible = true;
+         
+        
+           
+
+
+        }
+      
+
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Beep(100, 300))
+            {
+
+            }
+            Application.Exit();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            PlaySound("Z:\\musicc.wav");
+        }
+
+      
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.SetBounds(0, 0, Screen.PrimaryScreen.WorkingArea.Width, Screen.PrimaryScreen.WorkingArea.Height);
+            panel1.SetBounds(160, 0, this.Width - 160, this.Height - 5);
+            g = panel1.CreateGraphics();
+       
+        }
+        private void Desenare()
+        {
+            g.Clear(Color.Black);
+            Pen p = new Pen(Color.Black, 3);
+            int i;
+            string[] s1 = new string[] { "", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R" };
+            string[] s2 = new string[] { "", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20" };
+            Rectangle r = new Rectangle(158, 5, Width - 5, Height - 5);
+            SolidBrush b = new SolidBrush(Color.Black);
+            g.FillRectangle(b, r);
+            label1.Visible = false;
+        
+          
+
+            for (i = 25; i <= 691; i += 37)
+            {
+                p = new Pen(Color.White, 3);
+                g.DrawLine(p, new Point(i, 17), new Point(i, Height - 10));
+
+
+            }
+            for (i = 42; i <= 900; i += 37)
+            {
+                p = new Pen(Color.White, 1);
+                g.DrawLine(p, new Point(5, i), new Point(700, i));
+            }
+            Font f = new Font("Arial", 12, FontStyle.Bold);
+            for (i = 1; i <= 18; i++)
+            {
+                g.DrawString(s1[i], f, Brushes.White, 1 + i * 37, 10);
+                Thread.Sleep(50);
+            }
+            for (i = 1; i <= 18; i++)
+            {
+                g.DrawString(s2[i], f, Brushes.White, 1, 5 + 37 * i);
+                Thread.Sleep(50);
+            }
+
+            Jucator = 1;
+        }
+        private void jucator()
+        {
+            Jucator = 1;
+        }
+        private void jucator2()
+        {
+            Jucator = 2;
+        }
+        private int linie51()
+        {
+            int i, j, k, c;
+            for (i = 1; i <= nmax; i++)
+                for (j = 1; j <= nmax; j++)
+                    if (tab[i, j] == 1)
+                    {
+                        c = 1;
+                        if (j + 4 <= nmax)
+                            for (k = 1; k <= 4; k++)
+                                if (tab[i, j + k] == 1)
+                                    c++;
+                        if (c == 5)
+                            return 1;
+                        c = 1;
+                        if (i + 4 <= nmax)
+                            for (k = 1; k <= 4; k++)
+                                if (tab[i + k, j] == 1)
+                                    c++;
+                        if (c == 5)
+                            return 1;
+                        c = 1;
+                        if (i + 4 <= nmax && j + 4 <= nmax)
+                            for (k = 1; k <= 4; k++)
+                                if (tab[i + k, j + k] == 1)
+                                    c++;
+                        if (c == 5)
+                            return 1;
+                        c = 1;
+                        if (j - 5 >= 1 && i + 4 <= nmax)
+                            for (k = 1; k <= 4; k++)
+                                if (tab[i + k, j - k] == 1)
+                                    c++;
+                        if (c == 5)
+                            return 1;
+                    }
+            return 0;
+        }
+
+        private int linie52()
+        {
+            int i, j, k, c;
+            for (i = 1; i <= nmax; i++)
+                for (j = 1; j <= nmax; j++)
+                    if (tab[i, j] == 2)
+                    {
+                        c = 1;
+                        if (j + 4 <= nmax)
+                            for (k = 1; k <= 4; k++)
+                                if (tab[i, j + k] == 2)
+                                    c++;
+                        if (c == 5)
+                            return 1;
+                        c = 1;
+                        if (i + 4 <= nmax)
+                            for (k = 1; k <= 4; k++)
+                                if (tab[i + k, j] == 2)
+                                    c++;
+                        if (c == 5)
+                            return 1;
+                        c = 1;
+                        if (i + 4 <= nmax && j + 4 <= nmax)
+                            for (k = 1; k <= 4; k++)
+                                if (tab[i + k, j + k] == 2)
+                                    c++;
+                        if (c == 5)
+                            return 1;
+                        c = 1;
+                        if (j - 5 >= 1 && i + 4 <= nmax)
+                            for (k = 1; k <= 4; k++)
+                                if (tab[i + k, j - k] == 2)
+                                    c++;
+                        if (c == 5)
+                            return 1;
+                    }
+            return 0;
+        }
+
+
+        private void joc()
+        {
+            int ok = 0;
+
+            if (Jucator == 1)
+            {
+                if (linie51() == 1)
+                    ok = 1;
+            }
+            else
+                if (Jucator == 2)
+                    if (linie52() == 1)
+                        ok = 1;
+            if (ok == 1)
+            {
+                if (Beep(700, 1000))
+                {
+
+                }
+                MessageBox.Show("Jucatorul " + Jucator + " a castigat!");
+                initab();
+                Desenare();
+            }
+            else
+            {
+                if (Jucator == 1)
+                {
+                    jucator2();
+                    if (Beep(50, 300))
+                    {
+
+                    }
+                }
+                else
+                    if (Jucator == 2)
+                    {
+                        jucator();
+                        if (Beep(50, 300))
+                        {
+
+                        }
+                    }
+            }
+
+        }
+      
+
+        private void panel1_MouseClick_1(object sender, MouseEventArgs e)
+        {
+            int x = e.X;
+            int y = e.Y;
+            int i = (int)((float)(x - 25) / 37 + 1);
+            int j = (int)((float)(y - 42) / 37 + 1);
+
+            if (Jucator == 1)
+            {
+                tab[i, j] = 1;
+                SolidBrush b = new SolidBrush(Color.Blue);
+                g.FillEllipse(b, i * 37 - 4, j * 37 + 13, 25, 25);
+            }
+            else
+                if (Jucator == 2)
+                {
+                    tab[i, j] = 2;
+                    SolidBrush b = new SolidBrush(Color.Aquamarine);
+                    g.FillEllipse(b, i * 37 - 4, j * 37 + 13, 25, 25);
+                }
+            joc();
+            
+        }
+
+       
+
+      
+    }
+}
